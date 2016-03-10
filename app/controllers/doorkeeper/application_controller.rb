251cf40d07a46cc05c193bd226c2b55bd9835444
Aaditya S <jartek.91@gmail.com>
2016-03-10 08:15:55 +0530

    Update application_controller.rb

--- app/controllers/doorkeeper/application_controller.rb
+++ app/controllers/doorkeeper/application_controller.rb
@@ -2,9 +2,9 @@ module Doorkeeper
   class ApplicationController < ActionController::Base
     include Helpers::Controller
 
-    if ::Rails.version.to_i < 4 && !Rails.configuration.api_only
+    if ::Rails.version.to_i < 4 && !::Rails.configuration.api_only
       protect_from_forgery
-    elsif !Rails.configuration.api_only
+    elsif !::Rails.configuration.api_only
       protect_from_forgery with: :exception
     end
 
